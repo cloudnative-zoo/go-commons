e27c295ac0150146361cc148393a706d4f5891a3
hassnat <hassnatahmadpk@gmail.com>
2025-01-03 14:52:35 +0100

    improved dir

--- util/dir.go
+++ util/dir.go
@@ -1,14 +1,46 @@
 package util
 
 import (
-	"fmt"
 	"os"
+	"path/filepath"
 )
 
-func ValidateAndCreateDir(dir string) error {
-	if _, err := os.Stat(dir); os.IsNotExist(err) {
-		if err := os.MkdirAll(dir, 0o600); err != nil { //nolint:mnd
-			return fmt.Errorf("failed to create directory: %w", err)
+// CreateDir create new dir and sub dir if not exist.
+func CreateDir(dirPath ...string) error {
+	for _, dir := range dirPath {
+		newDir := filepath.Join(".", dir)
+		err := os.MkdirAll(newDir, 0o600) //nolint:mnd
+		if err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+// RemoveDir is removing dir.
+func RemoveDir(dirs ...string) error {
+	for _, dir := range dirs {
+		if dir != "" {
+			err := os.RemoveAll(dir)
+			if err != nil {
+				return err
+			}
+		}
+	}
+	return nil
+}
+
+// CleanDir is removing all files in dir.
+func CleanDir(dir string) error {
+	files, err := os.ReadDir(dir)
+	if err != nil {
+		return err
+	}
+
+	for _, file := range files {
+		err := os.Remove(filepath.Join(dir, file.Name()))
+		if err != nil {
+			return err
 		}
 	}
 	return nil
